febea37e11adff41d6f3638a9e6f8f72b66667d0
Samuel Arandia <samuel.arandia@urosario.edu.co>
2024-05-25 01:33:18 -0500

    avances

--- lib/helper_functions/helper_functions.dart
+++ lib/helper_functions/helper_functions.dart
@@ -1,9 +1,20 @@
 //ayuda
 
 //string a double
+import 'package:intl/intl.dart';
 
 double convertirStringaDouble(String string){
   double? valor = double.tryParse(string);
 
   return valor ?? 0;
+}
+
+
+//formato del valor
+
+
+
+String formatoValor(double valor) {
+  final formato = NumberFormat.currency(locale: 'en_US', symbol: "\$", decimalDigits: 2);
+  return formato.format(valor);
 }
