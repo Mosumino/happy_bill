6545760e89f87c8d5cc3be233b4c6b4dc9d7da5f
Samuel Arandia <samuel.arandia@urosario.edu.co>
2024-05-26 16:14:41 -0500

    cambios eliminar

--- lib/database/gasto_database.dart
+++ lib/database/gasto_database.dart
@@ -1,76 +1,63 @@
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:happy_bill/models/gasto.dart';
-class GastoDatabase extends ChangeNotifier{
-  static late Isar isar;
-  List<Gasto>_todoslosgastos = [];
 
-  //inicializando la base de datos
-  static Future<void>Inicializar() async{
-    final dir  = await getApplicationDocumentsDirectory();
+class GastoDatabase extends ChangeNotifier {
+  static late Isar isar;
+  List<Gasto> _todoslosgastos = [];
+
+  // Inicializando la base de datos
+  static Future<void> Inicializar() async {
+    final dir = await getApplicationDocumentsDirectory();
     isar = await Isar.open([GastoSchema], directory: dir.path);
   }
-  
- //creamos un getter para acceder a nuestra lista, ya que es privada
- List<Gasto> get todoslosgastos =>_todoslosgastos;
 
+  // Creamos un getter para acceder a nuestra lista, ya que es privada
+  List<Gasto> get todoslosgastos => _todoslosgastos;
 
-//OPERACIONES:
+  // OPERACIONES:
 
-//CREAR GASTO
-Future<void>crearGastoNuevo(Gasto nuevoGasto) async{
-  //añadir a la base de datos
-  await isar.writeTxn(() => isar.gastos.put(nuevoGasto));
+  // CREAR GASTO
+  Future<void> crearGastoNuevo(Gasto nuevoGasto) async {
+    // Añadir a la base de datos
+    await isar.writeTxn(() => isar.gastos.put(nuevoGasto));
 
-  //leer de nuevo el gasto
-  await leerGastos();
-
-
-}
-
-//LEER  GASTO
-
-Future<void> leerGastos() async {
-  //para obtener nuestros datos ya existentes
-  List<Gasto> fetchedGastos =  await isar.gastos.where().findAll();
-
-  //añadir a la lista de gastos
-  _todoslosgastos.clear();
-  _todoslosgastos.addAll(fetchedGastos);
-  
-
-  //modificar la interfaz gráfica
-  notifyListeners();
-}
-
-//Actualizar gasto
-Future<void>actualizarGasto(int id, Gasto actualizarGasto) async{
-  //ver si el nuevo gasto tiene el mismo id que el existente
-  actualizarGasto.id = id;
-
-  //actualizar en la base de datos
-  await isar.writeTxn(()=> isar.gastos.put(actualizarGasto));
-
-  //leerlo otra vez
-
-  await leerGastos();
-}
-  //eliminarlo 
-
-Future<void> eliminarGasto(int id) async{
-  //borrarlo de la base de datos 
-  await isar.writeTxn(() => isar.gastos.delete(id));
-
-  //leerlo de la base de datos 
-
-  await leerGastos();
-
-
+    // Leer de nuevo el gasto
+    await leerGastos();
   }
 
+  // LEER GASTO
+  Future<void> leerGastos() async {
+    // Para obtener nuestros datos ya existentes
+    List<Gasto> fetchedGastos = await isar.gastos.where().findAll();
 
+    // Añadir a la lista de gastos
+    _todoslosgastos.clear();
+    _todoslosgastos.addAll(fetchedGastos);
 
+    // Modificar la interfaz gráfica
+    notifyListeners();
+  }
 
+  // ACTUALIZAR GASTO
+  Future<void> actualizarGasto(int id, Gasto actualizarGasto) async {
+    // Ver si el nuevo gasto tiene el mismo id que el existente
+    actualizarGasto.id = id;
 
+    // Actualizar en la base de datos
+    await isar.writeTxn(() => isar.gastos.put(actualizarGasto));
+
+    // Leerlo otra vez
+    await leerGastos();
+  }
+
+  // ELIMINAR GASTO
+  Future<void> eliminarGasto(int id) async {
+    // Borrarlo de la base de datos
+    await isar.writeTxn(() => isar.gastos.delete(id));
+
+    // Leerlo de la base de datos
+    await leerGastos();
+  }
 }
